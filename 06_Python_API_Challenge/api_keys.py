ad9616feb3c40095ae4a139a70d28b8f2d416141
unknown <yxs158@miami.edu>
2021-01-01 12:50:46 -0500

    updated

--- 06_Python_API_Challenge/api_keys.py
+++ 06_Python_API_Challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "59fb390ae4963dee390790460b49af9b"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBVv6SjQIIKC9rvj3u8EAcvYYVmby063es"
+g_key = ""
